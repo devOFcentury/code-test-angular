42b9387e52aef2595b2c95c3d3e14d46a0163154
Henry Ibrahima Vincent COLY <vincoly7@gmail.com>
2024-04-14 20:38:39 +0000

    Layout of page elements and style the header, content and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
